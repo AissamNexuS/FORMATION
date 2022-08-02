8f2b519bae3ffe70c4abf63b88774cfea7c912db
ahmed.touti <ahmedtouti.r@gmail.com>
2022-08-02 13:43:42 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function hhhhhhhh() {
+  return (
+    <View>
+      <Text>hhhhhhhh</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
